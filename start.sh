5e43f295dd8b076f487094cb811347c95689bc70
gary <gary@MACSTATION.local>
2021-10-15 20:37:25 +0100

    Updates

--- start.sh
+++ start.sh
@@ -6,13 +6,25 @@
 if [ ! -d 'certs' ]; then
     git clone https://github.com/gary-archer/oauth.developmentcertificates ./certs
 fi
+if [ $? -ne 0 ]; then
+    echo 'Problem encountered downloading development certificates'
+    exit
+fi
 
 #
 # Build the app
 #
 mvn clean install
+if [ $? -ne 0 ]; then
+    echo 'Problem encountered building the API'
+    exit
+fi
 
 #
 # Then start listening
 #
 java -jar target/sampleapi-0.0.1-SNAPSHOT.jar
+if [ $? -ne 0 ]; then
+    echo 'Problem encountered running the API'
+    exit
+fi
