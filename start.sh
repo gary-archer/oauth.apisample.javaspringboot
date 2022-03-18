05f1af90e586a306a2e7b46c7add0ca0a50aee94
gary <gary@MACSTATION.local>
2022-03-18 18:54:48 +0000

    Updates

--- start.sh
+++ start.sh
@@ -9,9 +9,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 #
 # Download SSL certificates from a central repo if needed
 #
-if [ ! -d 'certs' ]; then
-    git clone https://github.com/gary-archer/oauth.developmentcertificates ./resources
-fi
+rm -rf ./resources
+git clone https://github.com/gary-archer/oauth.developmentcertificates ./resources
 if [ $? -ne 0 ]; then
     echo 'Problem encountered downloading development certificates'
     exit
