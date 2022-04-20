c267fe9a1c6d7d264d639d72fa2595e1bf99f627
gary <gary@MACSTATION.local>
2022-04-20 20:51:57 +0100

    Updates

--- src/test/java/com/mycompany/sample/tests/LoadTest.java
+++ src/test/java/com/mycompany/sample/tests/LoadTest.java
@@ -1,8 +1,8 @@
 package com.mycompany.sample.tests;
 
-import com.mycompany.sample.tests.utils.TokenIssuer;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.Suite;
+import com.mycompany.sample.tests.utils.TokenIssuer;
 
 /*
  * A basic load test to ensure that the API behaves correctly when there are concurrent requests
@@ -10,8 +10,8 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 public class LoadTest {
 
-    @Test
-    public void Run() throws Throwable {
+  @Test
+    public void run() throws Throwable {
 
         var issuer = new TokenIssuer();
         var accessToken = issuer.issueAccessToken("");
