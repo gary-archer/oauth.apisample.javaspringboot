aea2413fa8a564af51d90fd558438d25db90a962
gary <gary@MACSTATION.local>
2022-08-30 17:45:03 +0100

    Updates

--- deployment/kubernetes-local/deploy.sh
+++ deployment/kubernetes-local/deploy.sh
@@ -18,8 +18,8 @@ cp ../environments/kubernetes-local.config.json api.config.json
 #
 # Create a configmap for the API's JSON configuration file
 #
-kubectl -n deployed delete configmap api-config 2>/dev/null
-kubectl -n deployed create configmap api-config --from-file=api.config.json
+kubectl -n applications delete configmap api-config 2>/dev/null
+kubectl -n applications create configmap api-config --from-file=api.config.json
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the API configmap'
   exit 1
@@ -28,8 +28,8 @@ fi
 #
 # Create a secret for the private key password of the certificate file cert-manager will create
 #
-kubectl -n deployed delete secret finalapi-pkcs12-password 2>/dev/null
-kubectl -n deployed create secret generic finalapi-pkcs12-password --from-literal=password='Password1'
+kubectl -n applications delete secret finalapi-pkcs12-password 2>/dev/null
+kubectl -n applications create secret generic finalapi-pkcs12-password --from-literal=password='Password1'
 if [ $? -ne 0 ]; then
   echo '*** Problem encountered creating the API certificate secret'
   exit 1
@@ -38,8 +38,8 @@ fi
 #
 # Trigger deployment of the API to the Kubernetes cluster
 #
-kubectl -n deployed delete -f api.yaml 2>/dev/null
-kubectl -n deployed apply  -f api.yaml
+kubectl -n applications delete -f api.yaml 2>/dev/null
+kubectl -n applications apply  -f api.yaml
 if [ $? -ne 0 ]; then
   echo '*** API Kubernetes deployment problem encountered'
   exit 1
