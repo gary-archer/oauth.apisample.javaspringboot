4adb04e0d370065650aa83ed267da8b3ef6ba1c8
gary <garyarcher36@gmail.com>
2022-11-20 11:43:05 +0000

    Updates

--- run_wiremock.sh
+++ run_wiremock.sh
@@ -17,9 +17,16 @@ curl -O -s $WIREMOCK_DOWNLOAD_URL
 
 #
 # Run Wiremock over HTTPS in this terminal
+# On Linux ensure that you have first granted Java the permissions to listen on port 447:
+# - sudo setcap 'cap_net_bind_service=+ep' /usr/lib/jvm/zulu-17-amd64/bin/java
 #
-java -jar $WIREMOCK_JAR_FILE --https-port 447 --disable-http \
---https-keystore './certs/authsamples-dev.ssl.p12' --keystore-type 'pkcs12' --keystore-password 'Password1' --key-manager-password 'Password1'
+java -jar $WIREMOCK_JAR_FILE \
+--https-port 447 \
+--disable-http \
+--https-keystore './certs/authsamples-dev.ssl.p12' \
+--keystore-type 'pkcs12' \
+--keystore-password 'Password1' \
+--key-manager-password 'Password1'
 
 #
 # Prevent automatic terminal closure
