51c92893af3979e1a09683e5e062be398225fd38
gary <garyarcher36@gmail.com>
2023-06-04 09:09:27 +0100

    Updates

--- src/main/java/com/mycompany/sample/host/startup/HttpServerConfiguration.java
+++ src/main/java/com/mycompany/sample/host/startup/HttpServerConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
@@ -17,7 +16,6 @@ import com.mycompany.sample.plumbing.spring.CustomAuthorizationFilter;
  * A class to manage HTTP configuration for our server
  */
 @Configuration
-@EnableWebSecurity
 @SuppressWarnings(value = "checkstyle:DesignForExtension")
 public class HttpServerConfiguration {
 
