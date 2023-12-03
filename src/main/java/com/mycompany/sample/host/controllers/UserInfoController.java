0b89a731d867dbb2950f8841ef65f0b219296e7f
gary <gary.archer36@gmail.com>
2023-12-03 16:20:35 +0000

    Updates

--- src/main/java/com/mycompany/sample/host/controllers/UserInfoController.java
+++ src/main/java/com/mycompany/sample/host/controllers/UserInfoController.java
@@ -1,6 +1,5 @@
 package com.mycompany.sample.host.controllers;
 
-import java.util.concurrent.CompletableFuture;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,7 @@ import com.mycompany.sample.plumbing.dependencies.CustomRequestScope;
 
 /*
  * Return user info from the business data to the client
- * Clients call the authorization server's user info endpoint to get OAuth user attributes
+ * Clients instead call the authorization server's user info endpoint to get OAuth user attributes
  */
 @RestController
 @Scope(value = CustomRequestScope.NAME)
@@ -23,7 +22,8 @@ public class UserInfoController {
     private final ClaimsPrincipalHolder claimsHolder;
 
     /*
-     * Claims are injected into the controller after OAuth processing
+     * The claims holder may be injected into the controller before OAuth processing
+     * The OAuth filter then runs before any methods are called
      */
     public UserInfoController(final ClaimsPrincipalHolder claimsHolder) {
         this.claimsHolder = claimsHolder;
@@ -33,13 +33,13 @@ public class UserInfoController {
      * Return user attributes that are not stored in the authorization server that the UI needs
      */
     @GetMapping(value = "")
-    public CompletableFuture<ClientUserInfo> getUserInfo() {
+    public ClientUserInfo getUserInfo() {
 
         var extraClaims = (SampleExtraClaims) this.claimsHolder.getClaims().getExtraClaims();
 
         var userInfo = new ClientUserInfo();
         userInfo.setTitle(extraClaims.getTitle());
         userInfo.setRegions(extraClaims.getRegions());
-        return CompletableFuture.completedFuture(userInfo);
+        return userInfo;
     }
 }
--- src/main/java/com/mycompany/sample/host/startup/SecurityConfiguration.java
+++ src/main/java/com/mycompany/sample/host/startup/SecurityConfiguration.java
@@ -12,23 +12,23 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import com.mycompany.sample.plumbing.spring.CustomAuthorizationFilter;
 
 /*
- * A class to manage HTTP configuration for our server
+ * A class to manage OAuth security configuration for the REST API
  */
 @Configuration
 @SuppressWarnings(value = "checkstyle:DesignForExtension")
-public class HttpServerConfiguration {
+public class SecurityConfiguration {
 
     private final ConfigurableApplicationContext context;
 
-    public HttpServerConfiguration(final ConfigurableApplicationContext context) {
+    public SecurityConfiguration(final ConfigurableApplicationContext context) {
         this.context = context;
     }
 
     /*
      * Spring is a web backend technology stack by default, and activates lots of web related behaviour
-     * EnableWebSecurity by default results in cookie defenses, headers related to web hosting and so on
+     * These include cookie defenses, headers related to web hosting and so on
      * In my blog's architecture these concerns are dealt with in an API gateway or Web host, so I disable them here
-     * The API needs only to validate JWTs and apply authorization based on claims
+     * The API needs only to validate JWTs and implement claims based authorization
      */
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
@@ -40,7 +40,7 @@ public class HttpServerConfiguration {
                 .securityMatcher(new AntPathRequestMatcher(ResourcePaths.ALL))
                 .authorizeHttpRequests(authorize ->
 
-                        // This seems to be required in Spring Boot 3, to work around cryptic async errors
+                        // This seems to be required in Spring Boot 3, is using async calls during a request
                         // https://github.com/spring-projects/spring-security/issues/11962
                         authorize.dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll()
 
