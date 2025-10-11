b37b6d22040771fe89fac87d74a68d6c348071ea
Gary Archer <32388530+gary-archer@users.noreply.github.com>
2025-10-11 11:05:18 +0100

    Use --no-cache option in Docker build command

--- deployment/docker/build.sh
+++ deployment/docker/build.sh
@@ -27,7 +27,7 @@ fi
 #
 # Build the docker image
 #
-docker build -t finaljavaapi:latest .
+docker build --no-cache -t finaljavaapi:latest .
 if [ $? -ne 0 ]; then
   echo 'Problem encountered building the API docker image'
   exit
